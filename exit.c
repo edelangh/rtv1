8e29d16d271e70bf2c67830851e131537bf0bcc0
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-07-16 10:57:16 +0200

    Fixed error message colours

--- exit.c
+++ exit.c
@@ -5,20 +5,20 @@ void	ft_exit(int error)
 	if (error == 0)
 		exit(0);
 	else if (error == 1)
-		ft_putstr("Error :Not enouth info for spot");
+		ft_putstr("\e[31mError :Not enouth info for spot\e[0m");
 	else if (error == 2)
-		ft_putstr("Error :Too many spots !\n");
+		ft_putstr("\e[31mError :Too many spots !\n\e[0m");
 	else if (error == 3)
-		ft_putstr("{red}Error: invalide elem in file{eoc}\n");
+		ft_putstr("\e[31mError: invalide elem in file\n\e[0m");
 	else if (error == 4)
-		ft_putstr("{red}Error: can't open file\n{eoc}");
+		ft_putstr("\e[31mError: can't open file\n\e[0m");
 	else if (error == 5)
-		ft_putstr("{red}Error: can't read sizes of file\n{eoc}");
+		ft_putstr("\e[31mError: can't read sizes of file\n\e[0m");
 	else if (error == 6)
-		ft_putstr("{red}Error: too many objs\n{eoc}");
+		ft_putstr("\e[31mError: too many objs\n\e[0m");
 	else if (error == 7)
-		ft_putstr("Error: Too few arguments\n");
+		ft_putstr("\e[31mError: Too few arguments\n\e[0m");
 	else if (error == 8)
-		ft_putstr("Error: mlx init failed\n");
+		ft_putstr("\e[31mError: mlx init failed\n\e[0m");
 	exit(0);
 }
