68c9f8093a9b05481c4371d66bba79cc55d3d90b
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-08-15 09:53:58 +0200

    Defined error messages.

--- exit.c
+++ exit.c
@@ -17,21 +17,21 @@ void	ft_exit(int error)
 	if (error == 0)
 		exit(0);
 	else if (error == 1)
-		ft_putstr("\e[31mError :Not enouth info for spot\e[0m");
+		ft_putstr(ERR01);
 	else if (error == 2)
-		ft_putstr("\e[31mError :Too many spots !\n\e[0m");
+		ft_putstr(ERR02);
 	else if (error == 3)
-		ft_putstr("\e[31mError: invalide elem in file\n\e[0m");
+		ft_putstr(ERR03);
 	else if (error == 4)
-		ft_putstr("\e[31mError: can't open file\n\e[0m");
+		ft_putstr(ERR04);
 	else if (error == 5)
-		ft_putstr("\e[31mError: can't read sizes of file\n\e[0m");
+		ft_putstr(ERR05);
 	else if (error == 6)
-		ft_putstr("\e[31mError: too many objs\n\e[0m");
+		ft_putstr(ERR06);
 	else if (error == 7)
-		ft_putstr("\e[31mError: Too few arguments\n\e[0m");
+		ft_putstr(ERR07);
 	else if (error == 8)
-		ft_putstr("\e[31mError: mlx init failed\n\e[0m");
+		ft_putstr(ERR08);
 	exit(0);
 }
 
